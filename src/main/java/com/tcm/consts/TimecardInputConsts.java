0a97505cdd3d13e9fb83404ab289d278f4db8324
Your Name <you@example.com>
2021-08-19 20:53:08 +0900

    勤怠入力画面定数切り分け

---
+++ src/main/java/com/tcm/consts/TimecardInputConsts.java
@@ -0,0 +1,18 @@
+package com.tcm.consts;
+
+public interface TimecardInputConsts {
+
+	/** HTML */
+	public static final String TIMECARD_INPUT_HTML = "timecard-input";
+	/** data */
+	public static final String TIMECARD_INPUT_DATA = "timecardInputData";
+	/** 初期表示. */
+	public static final String ACTION_PATH_INIT = "init";
+	/** 更新処理. */
+	public static final String ACTION_PATH_UPDATE = "update";
+	/** 承認処理. */
+    public static final String ACTION_PATH_APPROVAL = "approval";
+	/** モーダル処理. */
+	public static final String ACTION_PATH_MODAL = "modal";
+
+}
